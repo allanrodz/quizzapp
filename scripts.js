13b520c983863320f6ec1ae9401887037d175f82
Francisco Lairton Rodolfo Calaca (ALLAN) <75245185+allanrodz@users.noreply.github.com>
2024-03-06 19:49:02 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,49 +7,64 @@ const videos = [
     // Add more video objects here
 ];
 
+
 function displayVideos(videoList) {
     const container = document.getElementById('videoContainer');
     container.innerHTML = '';
-    videoList.forEach(video => {
+    videoList.forEach((video, index) => {
         const videoItem = document.createElement('div');
         videoItem.classList.add('video-item');
-        
+
+        // Add line between videos (except for the last one)
+        if (index !== videoList.length) {
+            const line = document.createElement('hr');
+            videoItem.appendChild(line);
+        }
+
         // Creating title element
         const titleElement = document.createElement('h6');
         titleElement.innerText = video.title;
+        titleElement.classList.add('animated-title'); // Add class for animated title
         titleElement.style.fontWeight = 'bold';
         titleElement.style.marginBottom = '5px';
-        titleElement.style.backgroundColor = 'black';
         titleElement.style.color = 'orange';
-
         videoItem.appendChild(titleElement);
         
+        // Always add line after the title
+        const line = document.createElement('hr');
+        videoItem.appendChild(line);
+
         // Creating iframe element
         const iframe = document.createElement('iframe');
         iframe.setAttribute('src', video.src);
         iframe.setAttribute('width', '100%');
-        iframe.setAttribute('height', '100%');
+        iframe.setAttribute('height', '100%'); // Set height to 100vh
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute('allow', 'autoplay; fullscreen');
         iframe.setAttribute('allowfullscreen', '');
         videoItem.appendChild(iframe);
-        
+
         container.appendChild(videoItem);
     });
 }
 
 
 
-function searchVideos() {
-    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
-    const filteredVideos = videos.filter(video => video.title.toLowerCase().includes(searchTerm));
-    displayVideos(filteredVideos);
-}
+
 
 function filterVideos(type) {
     const filteredVideos = videos.filter(video => video.type === type);
     displayVideos(filteredVideos);
 }
+
+function filterByKeyword(keyword) {
+    const filteredVideos = videos.filter(video => video.title.toLowerCase().includes(keyword.toLowerCase()));
+    displayVideos(filteredVideos);
+}
+
+document.getElementById('searchInput').addEventListener('input', function(event) {
+    filterByKeyword(event.target.value);
+});
 
 // Display all videos initially
 displayVideos(videos);
